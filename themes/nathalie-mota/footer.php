fca692cdc89d6637e24425aeb0b49f22a7a10314
Stephane Mouron <stephane.mouron@free.fr>
2024-12-30 18:56:16 +0100

    Ajout d'un peu de couleur

--- themes/nathalie-mota/footer.php
+++ themes/nathalie-mota/footer.php
@@ -7,14 +7,23 @@
  */
 ?>
 	<footer id="footer">
-		<?php 
-			// Affichage du menu footer déclaré dans functions.php
-			wp_nav_menu(array('theme_location' => 'footer')); 
-		?>		
-		<!-- Ajout du widget dans le pied de page -->	
-		<aside id="widget-area" >
-			<?php dynamic_sidebar( 'footer-widget' ); ?>
-		</aside>
+		<div id="top-footer">
+			<?php 
+				// Affichage du menu footer déclaré dans functions.php
+				wp_nav_menu(array('theme_location' => 'footer')); 
+			?>		
+			<!-- Ajout du widget dans le pied de page -->	
+			<aside id="widget-area" >
+				<?php dynamic_sidebar( 'footer-widget' ); ?>
+			</aside>
+		</div>
+		<div id="bottom-footer">
+			<p>Créé par 
+				<a href="https://stephane-mouron.fr/" target="_blank" rel="noopener noreferrer">Stéphane&nbsp;Mouron</a>
+				 de 
+				<a href="https://sanoecreation.fr/" target="_blank" rel="noopener noreferrer">Sanoé&nbsp;Création</a>
+			</p>
+		</div>
 	</footer>
 
 	<!-- Lance la popup contact -->
